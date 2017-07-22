9408d1eb2450fc22de9fb3138d6d842d37806b5a
Ulrich Schmidt <admin@deruli.de>
2017-07-22 18:10:28 +0200

    erster commit

---
+++ src/content/modules/glossary/Glossary.php
@@ -0,0 +1,22 @@
+<?php
+
+class Glossary
+{
+
+    private $moduleName = "glossary";
+
+    public function getSettingsHeadline()
+    {
+        return get_translation("glossary");
+    }
+
+    public function getSettingsLinkText()
+    {
+        return get_translation("edit");
+    }
+
+    public function settings()
+    {
+        return Template::executeModuleTemplate($this->moduleName, "glossary_list.php");
+    }
+}
---
+++ src/content/modules/glossary/templates/glossary_list.php
@@ -0,0 +1,12 @@
+<table>
+	<thead>
+		<tr>
+			<th>Titel</th>
+			<td><strong><?php translate("view");?></strong></td>
+			<td><strong><?php translate("edit");?></strong></td>
+			<td><strong><?php translate("delete");?></strong></td>
+		</tr>
+	</thead>
+	<tbody>
+	</tbody>
+</table>
